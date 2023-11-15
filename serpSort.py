60e5902eaac8ce68d558346c094b64e6150d5146
unknown <stangarrow@yahoo.com>
2023-11-15 07:42:23 -0800

    initial commit.

--- serpSort.py
+++ serpSort.py
@@ -1,21 +1,20 @@
 def loadData():
-    serpInput = []
+    serpDatLst = []
     f = open('serp_input.csv')
-    serpHeader = f.readline().strip().split(',')
+    serpHdrLst = f.readline().strip().split(',')
     for line in f:
-        serpInputLine = line.strip().split(',')
-        serpInput.append([int(x) for x in serpInputLine] )
+        serpDatLine = line.strip().split(',')
+        serpDatLst.append([int(x) for x in serpDatLine] )
     f.close
-    return serpHeader, serpInput[0:11]
-    #return serpHeader, serpInput
+    #return serpHdrLst, serpDatLst[0:11]  # return only first few ... for debug.
+    return serpHdrLst, serpDatLst       # return all for non-debug.
 #############################################################################
 
-def rangeData(serpHeader,  serpInput):
-    import pprint as pp
+def rangeData(serpHdrLst, serpDatLst):
     minMaxLst = []
-    for hIdx,hdr in enumerate(serpHeader):
+    for hIdx,hdr in enumerate(serpHdrLst):
         valLst = []
-        for vals in serpInput:
+        for vals in serpDatLst:
             valLst.append(vals[hIdx])
         minVal = min(valLst)
         maxVal = max(valLst)
@@ -23,69 +22,76 @@ def rangeData(serpHeader,  serpInput):
     return minMaxLst
 #############################################################################
 
-def genSerpSearchCriteriaLst( minMaxLst ):
+def genSerpSearchCriteriaLst(minMaxLst):
     import math
-    
+
     diffs       = [b-a+1 for a,b in minMaxLst]
     numToGen    = math.prod(diffs)
-    should      = [ 'inc' ] * len(minMaxLst)
-    currVals    = [ x[0] for x in minMaxLst ]
+    should      = ['inc'] * len(minMaxLst)
+    currVals    = [x[0] for x in minMaxLst]
     serpSrchLst = []
-    
+
     while len(serpSrchLst) < numToGen:
 
-        serpSrchLst.append(currVals[:])
+        serpSrchLst.append(currVals[:]) # <-- the ':' fixed a bug.
 
-        for idx in range( len(currVals)-1, -1, -1 ):
+        for idx in range(len(currVals)-1, -1, -1):
             if should[idx] == 'inc':
                 currVals[idx] += 1
                 if currVals[idx]  > minMaxLst[idx][1]:
                     currVals[idx] = minMaxLst[idx][1]
                     should[idx] = 'dec'
                     continue
-                
+
             if should[idx] == 'dec':
                 currVals[idx] -= 1
                 if currVals[idx]  < minMaxLst[idx][0]:
                     currVals[idx] = minMaxLst[idx][0]
                     should[idx] = 'inc'
                     continue
-                    
-            break
-       
-    return serpSrchLst
-#############################################################################    
 
-def genSerpSortedDataLst( serpSearchCriteriaLst, serpData ):
+            break
+
+    return serpSrchLst
+#############################################################################
+
+def genSerpSortedDataLst(serpSearchCriteriaLst, serpDataLst):
     serpSortedDataLst = []
     for critera in serpSearchCriteriaLst:
-        for data in serpData:
-            print('***', critera, data)
+        for data in serpDataLst:
             if data == critera:
-                print('append')
                 serpSortedDataLst.append(data)
     return serpSortedDataLst
-#############################################################################    
+#############################################################################
+
+def writeSerpToFile(serpHdr, serpDatLst):
+    with open('serp_output.txt', 'w') as f:
+        for val in serpHdr:
+            f.write('{} '.format(val))
+        f.write('\n')
+        for vals in serpDatLst:
+            for val in vals:
+                f.write('{:2} '.format(val))
+            f.write('\n')
+    return
+#############################################################################
 
 if __name__ == '__main__':
     import pprint as pp
 
-    serpHeader, serpData = loadData()
-    minMaxLst = rangeData(serpHeader, serpData)
-
+    serpHdr, serpDatLst = loadData()
+    minMaxLst = rangeData(serpHdr, serpDatLst)
     serpSearchCriteriaLst = genSerpSearchCriteriaLst(minMaxLst)
-    serpSortedDataLst = genSerpSortedDataLst(serpSearchCriteriaLst, serpData)
+    serpSortedDatLst = genSerpSortedDataLst(serpSearchCriteriaLst, serpDatLst)
+    writeSerpToFile(serpHdr, serpSortedDatLst)
 
-    print()
-    #pp.pprint(serpSearchCriteriaLst)
-    print()
-    
-    print()
-    pp.pprint(serpData)
-    print()
-
-    print()
-    pp.pprint(serpSortedDataLst)
-    print()
-
-    print(len(serpData), len(serpSortedDataLst))
+    #debug = True
+    debug = False
+    if debug:
+        thingsToPprint = [serpDatLst, serpSortedDatLst]
+        #thingsToPprint = [serpSearchCriteriaLst, serpDatLst, serpSortedDatLst]
+        for x in thingsToPprint:
+            print()
+            pp.pprint(x)
+            print()
+        print(len(serpDatLst), len(serpSortedDatLst))
